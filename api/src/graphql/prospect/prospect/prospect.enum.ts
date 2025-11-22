5526ac3443a7de5c7a18e6860228c256d5361819
Happykiller <fabrice.rosito@gmail.com>
2025-11-22 18:25:35 +0100

    Fix prospect status enum typing

--- api/src/graphql/prospect/prospect/prospect.enum.ts
+++ api/src/graphql/prospect/prospect/prospect.enum.ts
@@ -2,6 +2,7 @@ import { registerEnumType } from '@nestjs/graphql';
 
 import { ProspectStatus } from '@src/common/prospect-status.enum';
 
+export type ProspectStatusEnum = ProspectStatus;
 export const ProspectStatusEnum = ProspectStatus;
 
 registerEnumType(ProspectStatusEnum, { name: 'ProspectStatusEnum' });
