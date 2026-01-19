59807b822a13986faa5c522e2db618e1d5dff642
Happykiller <fabrice.rosito@gmail.com>
2026-01-19 20:59:42 +0100

    Add dividers in program record view

--- frontoffice/src/pages/program-records/components/ProgramRecordExerciseCardReadOnly.tsx
+++ frontoffice/src/pages/program-records/components/ProgramRecordExerciseCardReadOnly.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import {
+    Divider,
     Grid,
     Paper,
     Stack,
@@ -43,44 +44,48 @@ export function ProgramRecordExerciseCardReadOnly({
                     {exerciseIndex + 1}. {exerciseLabel}
                 </Typography>
                 {sets.length > 0 ? (
-                    <Stack spacing={1.5}>
-                        {sets.map((set) => (
-                            <Stack spacing={1} key={`${exerciseLabel}-set-${set.index}`}>
-                                <Typography variant="body2" fontWeight={600}>
-                                    {t('program_record.form.series_label', { index: set.index })}
-                                </Typography>
-                                <Grid container spacing={1.5}>
-                                    <Grid size={{ xs: 6, sm: 3 }}>
-                                        <Typography variant="caption" color="text.secondary">
-                                            {t('program_record.form.repetitions_label')}
-                                        </Typography>
-                                        <Typography variant="body2">{set.repetitions ?? '-'}</Typography>
+                    <>
+                        <Divider />
+                        <Stack spacing={1.5}>
+                            {sets.map((set) => (
+                                <Stack spacing={1} key={`${exerciseLabel}-set-${set.index}`}>
+                                    <Typography variant="body2" fontWeight={600}>
+                                        {t('program_record.form.series_label', { index: set.index })}
+                                    </Typography>
+                                    <Grid container spacing={1.5}>
+                                        <Grid size={{ xs: 6, sm: 3 }}>
+                                            <Typography variant="caption" color="text.secondary">
+                                                {t('program_record.form.repetitions_label')}
+                                            </Typography>
+                                            <Typography variant="body2">{set.repetitions ?? '-'}</Typography>
+                                        </Grid>
+                                        <Grid size={{ xs: 6, sm: 3 }}>
+                                            <Typography variant="caption" color="text.secondary">
+                                                {t('program_record.form.charge_label')}
+                                            </Typography>
+                                            <Typography variant="body2">{set.charge ?? '-'}</Typography>
+                                        </Grid>
+                                        <Grid size={{ xs: 6, sm: 3 }}>
+                                            <Typography variant="caption" color="text.secondary">
+                                                {t('program_record.form.rpe_label')}
+                                            </Typography>
+                                            <Typography variant="body2">{set.rpe ?? '-'}</Typography>
+                                        </Grid>
+                                        <Grid size={{ xs: 6, sm: 3 }}>
+                                            <Typography variant="caption" color="text.secondary">
+                                                {t('program_record.form.done_label')}
+                                            </Typography>
+                                            <Typography variant="body2">
+                                                {set.done ? t('common.yes') : t('common.no')}
+                                            </Typography>
+                                        </Grid>
                                     </Grid>
-                                    <Grid size={{ xs: 6, sm: 3 }}>
-                                        <Typography variant="caption" color="text.secondary">
-                                            {t('program_record.form.charge_label')}
-                                        </Typography>
-                                        <Typography variant="body2">{set.charge ?? '-'}</Typography>
-                                    </Grid>
-                                    <Grid size={{ xs: 6, sm: 3 }}>
-                                        <Typography variant="caption" color="text.secondary">
-                                            {t('program_record.form.rpe_label')}
-                                        </Typography>
-                                        <Typography variant="body2">{set.rpe ?? '-'}</Typography>
-                                    </Grid>
-                                    <Grid size={{ xs: 6, sm: 3 }}>
-                                        <Typography variant="caption" color="text.secondary">
-                                            {t('program_record.form.done_label')}
-                                        </Typography>
-                                        <Typography variant="body2">
-                                            {set.done ? t('common.yes') : t('common.no')}
-                                        </Typography>
-                                    </Grid>
-                                </Grid>
-                            </Stack>
-                        ))}
-                    </Stack>
+                                </Stack>
+                            ))}
+                        </Stack>
+                    </>
                 ) : null}
+                <Divider />
                 <Stack spacing={0.5}>
                     <Typography variant="body2" fontWeight={600}>
                         {t('program_record.form.exercise_notes_label')}
