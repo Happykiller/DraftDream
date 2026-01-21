8dbb28fb5acbdd94fbbc28442ada6fc790ec7160
Happykiller <fabrice.rosito@gmail.com>
2026-01-21 15:54:57 +0100

    Use RestaurantMenu icon for nutrition plans

--- backoffice/src/components/dashboard/widgets/home/NutritionPlansWidget.tsx
+++ backoffice/src/components/dashboard/widgets/home/NutritionPlansWidget.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTheme } from '@mui/material';
-import { Restaurant } from '@mui/icons-material';
+import { RestaurantMenu } from '@mui/icons-material';
 import { useTranslation } from 'react-i18next';
 
 import { DistributionChartWidget } from '@components/dashboard/widgets/DistributionChartWidget';
@@ -41,7 +41,7 @@ export const NutritionPlansWidget: React.FC<NutritionPlansWidgetProps> = ({
                 title={t('home.widgets.nutrition_plans')}
                 tooltip={t('home.widgets.nutrition_plans_tooltip')}
                 value={totalMealPlans}
-                icon={Restaurant}
+                icon={RestaurantMenu}
                 colSpan={1}
                 color={theme.palette.secondary.main}
                 data={distributionData}
